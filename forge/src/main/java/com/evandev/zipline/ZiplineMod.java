4690acd74e9664e5f35aa2ec2fa3b5c1b707d6b1
evanbones <74276616+evanbones@users.noreply.github.com>
2026-04-17 12:45:11 -0700

    1.1.1

--- forge/src/main/java/com/evandev/zipline/ZiplineMod.java
+++ forge/src/main/java/com/evandev/zipline/ZiplineMod.java
@@ -3,12 +3,14 @@ package com.evandev.zipline;
 import com.evandev.zipline.client.ClientConfigSetup;
 import com.evandev.zipline.client.ZiplineClient;
 import com.evandev.zipline.platform.ForgeRegistryHelper;
+import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.fml.loading.FMLLoader;
 
 @Mod(Zipline.MOD_ID)
 public class ZiplineMod {
@@ -22,6 +24,10 @@ public class ZiplineMod {
 
         modEventBus.addListener(this::commonSetup);
         modEventBus.addListener(this::clientSetup);
+
+        if (FMLLoader.getDist() == Dist.CLIENT) {
+            ClientConfigSetup.register();
+        }
 
         MinecraftForge.EVENT_BUS.register(this);
     }
